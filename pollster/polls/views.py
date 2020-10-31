e8143871b9c4fd244c6420c134c84ad87649bf3c
prabodh44 <prabodhtuladhar44@gmail.com>
2020-10-31 20:57:10 +0545

    Added some code to views.py | created urls.py in polls folder
    

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Question, Choice
 # Create your views here.
+
+# Get questions and display them
+def index(request):
+    return render(request, 'polls/index.html')
