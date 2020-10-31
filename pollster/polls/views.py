0dac11feac73c773301caf5f8ed8bb5f66657cb1
prabodh44 <prabodhtuladhar44@gmail.com>
2020-10-31 21:46:56 +0545

    added details and results views | added url routes to details and results

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -5,4 +5,27 @@ from .models import Question, Choice
 
 # Get questions and display them
 def index(request):
-    return render(request, 'polls/index.html')
+    # shows five questions in descending order of date
+    # minus sign(-) signifies descending order
+    latest_question_list = Question.objects.order_by('-pub_date')[:5] 
+    context = {
+        'latest_question_list':latest_question_list,
+    }    
+    return render(request, 'polls/index.html', context)
+
+
+# show specific questions and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise http404("Question does not exist")
+    return(render(request, "polls/details.html", {'question' : question}))
+    
+
+# get question and display results
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question' : question_id})
+
